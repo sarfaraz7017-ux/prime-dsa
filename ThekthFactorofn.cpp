47c511f3396b5402476f6f305796a72f56c4c5ea
Sarfaraz Ansari <ansarisarfaraj7017@gmail.com>
2026-02-11 13:47:50 +0530

    ADD

---
+++ ThekthFactorofn.cpp
@@ -0,0 +1,19 @@
+# include<iostream>    
+using namespace std;
+int kthFactor(int n, int k) {
+        int cnt=0;
+        for(int i=1;i<=n;i++){
+            if(n%i==0){
+                cnt++;
+                if(cnt==k){
+                    return i;
+                }
+            }
+        }
+        return -1;  
+    }
+int main(){
+    int n, k;
+    cin >> n >> k;
+    cout << kthFactor(n, k) << endl;
+}
--- pattern.cpp
+++ pattern.cpp
@@ -8,7 +8,7 @@ int main() {
     for(int i=1;i<=n;i++){
         //space
         for(int j=1;j<=n-i;j++){
-            cout<<" \t";
+            cout<<" \t"; 
         }
         //pattern
         for(int j=1;j<=2*i-1;j++){
