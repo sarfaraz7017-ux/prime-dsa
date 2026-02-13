e12a94dd309ba55d87ce0aeb07970d941d3640d5
Sarfaraz Ansari <ansarisarfaraj7017@gmail.com>
2026-02-13 22:43:08 +0530

    add

--- addtwonumber.cpp
+++ addtwonumber.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main() {
-   int A,B,sum;
-   cout<<"enter A";
-   cin>>A;
-   cout<<"enter B";
-   cin>>B;
-   sum=A+B;
-   cout<<"sum ="<<sum;
+int main()
+{
+    int A, B, sum;
+    cout << "enter A";
+    cin >> A;
+    cout << "enter B";
+    cin >> B;
+    sum = A + B;
+    cout << "sum =" << sum;
     return 0;
 }
-
