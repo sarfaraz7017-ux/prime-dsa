1fda7e376f09e86008e0d0fd4c0522419dea1d97
Sarfaraz Ansari <ansarisarfaraj7017@gmail.com>
2026-02-17 15:46:23 +0530

    add

---
+++ invers.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main() {
+	int num;
+	cin>>num;
+	int temp = num;
+	int count= 0;
+
+	while(temp != 0){		//counting total digits in num
+		count++;
+		temp /= 10;
+    }
+	vector <int> ans(count);//initializing the size of vector acc to digits
+	int i = 0;
+	while(num != 0){
+		int l = num % 10;
+		ans[l-1] =i+1;
+		i++;
+		num /= 10;
+	}
+	for(int i =count-1; i>=0 ; i--){
+		cout<<ans[i];
+	}
+	return 0;
+}
