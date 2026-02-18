e22ab265c4262daacb7dc743b2cfe30aedcbf4d3
Sarfaraz Ansari <ansarisarfaraj7017@gmail.com>
2026-02-18 14:38:51 +0530

    add

--- bubbleshort.cpp
+++ bubbleshort.cpp
@@ -2,8 +2,9 @@
 using namespace std;
 int main(){
     int n;
-    cin>>n;
+    cin>>n; 
     int arr[n];
+    cout<<"\n";
     for(int i=0;i<n;i++){
         cin>>arr[i];
     }
@@ -18,4 +19,5 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+    
 }
