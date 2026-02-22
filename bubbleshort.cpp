6240e3aeec061a983cef6eec44fdac7644f8f299
Sarfaraz Ansari <ansarisarfaraj7017@gmail.com>
2026-02-22 16:39:44 +0530

    add

--- bubbleshort.cpp
+++ bubbleshort.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 using namespace std;
-int main(){
+int main()
+{
     int n;
-    cin>>n; 
+    cin >> n;
     int arr[n];
-    cout<<"\n";
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    cout << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-    for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-1-i;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
             }
         }
     }
-    cout<<"sorted array to bubble short is : ";
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<" ";
+    cout << "sorted array to bubble short is : ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
-    
 }
