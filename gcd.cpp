88974e68387545bf9b9b34ee6c8293034ebd8f32
Sarfaraz Ansari <ansarisarfaraj7017@gmail.com>
2026-02-22 16:40:57 +0530

    add

--- gcd.cpp
+++ gcd.cpp
@@ -1,17 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int gcd(int a , int b){
-	if(b == 0){
+int gcd(int a, int b)
+{
+	if (b == 0)
+	{
 		return a;
-	}else{
-		return gcd(b, a%b);
+	}
+	else
+	{
+		return gcd(b, a % b);
 	}
 }
-int main() {
+int main()
+{
 	int a, b;
-	cin>>a>>b;
-	cout<<gcd(a,b);
+	cin >> a >> b;
+	cout << gcd(a, b);
 
 	return 0;
 }
