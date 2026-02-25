0adafeb8db47198f08072ca7e3b38f167b3ba7a3
Sarfaraz Ansari <ansarisarfaraj7017@gmail.com>
2026-02-25 13:32:08 +0530

    add

--- gasstation.cpp
+++ gasstation.cpp
@@ -1,24 +1,29 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 using namespace std;
-int startStation(vector<int> gas,vector<int> cost){
-    int total=0;
-    int bachihuigas=0;
-    int ans=0;
-    for(int i=0;i<gas.size();i++){
-        total+=(gas[i]-cost[i]);
-        bachihuigas+=(gas[i]-cost[i]);
-        if(bachihuigas<0){
-            bachihuigas=0;
-            ans=i+1;
+int startStation(vector<int> gas, vector<int> cost)
+{
+    int total = 0;
+    int bachihuigas = 0;
+    int ans = 0;
+    for (int i = 0; i < gas.size(); i++)
+    {
+        total += (gas[i] - cost[i]);
+        bachihuigas += (gas[i] - cost[i]);
+        if (bachihuigas < 0)
+        {
+            bachihuigas = 0;
+            ans = i + 1;
         }
     }
-    if(total>=0){
+    if (total >= 0)
+    {
         return ans;
     }
     return -1;
 }
-int main() {
-    
+int main()
+{
+
     return 0;
 }
