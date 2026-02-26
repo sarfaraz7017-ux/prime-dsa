cfaff81fb1aaf9cc05e0b8286fa3d20e6b071afe
Sarfaraz Ansari <ansarisarfaraj7017@gmail.com>
2026-02-26 15:17:15 +0530

    add

--- 2D_array.cpp
+++ 2D_array.cpp
@@ -4,7 +4,7 @@ int main(){
    int array[3][4]={
     {1,2,3,4},
     {5,6,7,8},
-    {9,10,11,12}
+    {9,10,11,12} 
    };
    int a[2][3]={1,2,3,4,5,6};
    int m,n;
@@ -16,5 +16,11 @@ int main(){
     }
     cout<<endl;
    }
-    return 0;
+   for (int i=0;i<2;i++){
+    for(int j=0;j<3;j++){
+        cout<<a[i][j]<<" ";
+    }
+    cout<<endl;
+   }
+   return 0;
 }
